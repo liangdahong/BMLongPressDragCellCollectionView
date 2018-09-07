90de2a8f50d8e795e21aa401049a3eb3f9f77d2b
appmj <3441045300@qq.com>
2018-09-07 11:21:49 +0800

    s

--- BMDragCollectionView/Demo/Alipay2VC/Controller/BMAlipay2VC.h
+++ BMDragCollectionView/Demo/Alipay2VC/Controller/BMAlipay2VC.h
@@ -2,16 +2,12 @@
 //  BMAlipayVC.h
 //  BMDragCellCollectionViewDemo
 //
-//  Created by ___liangdahong on 2017/8/22.
-//  Copyright © 2017年 https://liangdahong.com All rights reserved.
-//
+
 
 #import <UIKit/UIKit.h>
 #import "UICollectionView+BMTemplateLayoutCell.h"
 #import "BMAlipay2HeaderView.h"
 #import "BMAlipay2FooterView.h"
-
-
 
 @interface BMAlipay2VC : UIViewController
 
