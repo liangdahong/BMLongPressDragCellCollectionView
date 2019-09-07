90776431b00d39254e05805c8687d2b13fc1015a
appmj <3441045300@qq.com>
2019-09-07 11:23:14 +0800

    s

--- BMDragCollectionView/Demo/Alipay2VC/Controller/BMAlipay2VC.h
+++ BMDragCollectionView/Demo/Alipay2VC/Controller/BMAlipay2VC.h
@@ -6,6 +6,8 @@
 //  Copyright © 2017年 https://liangdahong.com All rights reserved.
 //
 
+
+
 #import <UIKit/UIKit.h>
 #import "UICollectionView+BMTemplateLayoutCell.h"
 #import "BMAlipay2HeaderView.h"
